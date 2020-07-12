c5d60b0610f4a6b9c30695783583306e5fbce748
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-07-11 16:45:11 -1000

    app.delete notes saving with ID but not deleting

--- server.js
+++ server.js
@@ -11,10 +11,13 @@ const PORT = 4500;
 const mainPath = path.join(__dirname, "/public")
 
 // Sets up the Express app to handle data parsing
+// =============================================================
 app.use(express.json());
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }));
 
+// ROUTES
+// =============================================================
 app.get("/notes", (req, res)=>{
     res.sendFile(path.join(mainPath,"notes.html"));
 });
@@ -28,10 +31,14 @@ app.get("/api/notes/:id", (req, res)=>{
     res.json(savedNotes[Number(req.params.id)]);
 });
 
+// DEFAULTS TO HOME PAGE, PREVENTS USER FROM WRITING AN INVALID PATH.
+// =============================================================
 app.get("*", (req, res)=>{
     res.sendFile(path.join(mainPath, "index.html"));
 });
 
+//  NOTES SAVED INTO DB.JSON
+// =============================================================
 app.post("/api/notes", (req, res)=> {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNotes = req.body;
@@ -40,9 +47,20 @@ app.post("/api/notes", (req, res)=> {
     savedNotes.push(newNotes);
 
     fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-    console.log("note saved to db.json. Content: ", newNotes);
+    console.log("note saved to db.json: ", newNotes);
     res.json(savedNotes)
-})
+});
+
+//  DELETES NOTE WITH ID FROM DB.JSON
+// =============================================================
+app.delete('/api/notes/:id', (req, res)=>{
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json","utf8"));
+   console.log(savedNotes)
+  
+
+fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+res.json(savedNotes);
+});
 
 // Starts the server to begin listening
 // =============================================================
